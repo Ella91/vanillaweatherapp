3cfa0e846d79856da646dac6c0ca885d1f3a14c2
Ella91 <75183157+Ella91@users.noreply.github.com>
2021-01-07 13:45:35 +0100

    Finalized Style

--- src/index.js
+++ src/index.js
@@ -59,15 +59,15 @@ function displayForecast(response) {
         forecastElement.innerHTML += `
             <div class="col-2">
                 <h3>
-                    ${formatHours(forecast.dt * 1000)}
+                ${formatHours(forecast.dt * 1000)}
                 </h3>
                 <img 
                 src= "http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
                 <div class="weather-forecast-temperature">
-                    <strong>
-                    ${Math.round(forecast.main.temp_max)}°
-                    </strong> | 
-                    ${Math.round(forecast.main.temp_min)}°
+                <strong>
+                ${Math.round(forecast.main.temp_max)}°
+                </strong> | 
+                ${Math.round(forecast.main.temp_min)}°
                 </div>
             </div>
             `;}
